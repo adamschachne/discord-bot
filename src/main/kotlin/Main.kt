54dd3f6a78710f016d337e2fb3f489e10c67fa6a
Adam Schachne <adamschachne@live.com>
2019-08-17 19:39:27 -0700

    read local properties and start bot

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,26 @@
+import net.dv8tion.jda.api.AccountType
+import net.dv8tion.jda.api.JDABuilder
+import java.io.FileInputStream
+import java.util.Properties
+import javax.security.auth.login.LoginException
+import kotlin.system.exitProcess
+
 fun main() {
-    println("hello discord")
+    val properties = Properties()
+    val propertiesFile = System.getProperty("user.dir") + "\\local.properties"
+    val inputStream = FileInputStream(propertiesFile)
+
+    properties.load(inputStream)
+
+    val token = properties.getProperty("token") ?: ""
+
+    try {
+        JDABuilder(AccountType.BOT)
+            .setToken(token)
+            .addEventListeners(Bot())
+            .build()
+    } catch (e: LoginException) {
+        System.err.println(e.message)
+        exitProcess(1)
+    }
 }

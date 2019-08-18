f2c5e11e405b6a542a3fd5859880c1f946a4f6de
Adam Schachne <adamschachne@live.com>
2019-08-17 22:47:11 -0700

    single jar

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,30 @@
 import net.dv8tion.jda.api.AccountType
 import net.dv8tion.jda.api.JDABuilder
 import java.io.FileInputStream
+import java.io.FileNotFoundException
+import java.lang.Exception
 import java.util.Properties
-import javax.security.auth.login.LoginException
 import kotlin.system.exitProcess
 
 fun main() {
-    val properties = Properties()
-    val propertiesFile = System.getProperty("user.dir") + "\\local.properties"
-    val inputStream = FileInputStream(propertiesFile)
-
-    properties.load(inputStream)
-
-    val token = properties.getProperty("token") ?: ""
-
     try {
+        val properties = Properties()
+        val propertiesFile = System.getProperty("user.dir") + "\\local.properties"
+        val inputStream = FileInputStream(propertiesFile)
+
+        properties.load(inputStream)
+
+        val token = properties.getProperty("token") ?: ""
+
         JDABuilder(AccountType.BOT)
             .setToken(token)
             .addEventListeners(Bot())
             .build()
-    } catch (e: LoginException) {
-        System.err.println(e.message)
+    } catch (e: Exception) {
+        when (e) {
+            is FileNotFoundException -> System.err.println("Could not find local.properties")
+            else -> System.err.println(e.message)
+        }
         exitProcess(1)
     }
 }

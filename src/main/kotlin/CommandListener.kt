906fe9e2601bd55494873bc91affe972c36c8852
Adam Schachne <adamschachne@live.com>
2019-08-18 17:22:56 -0700

    timeout

--- src/main/kotlin/CommandListener.kt
+++ src/main/kotlin/CommandListener.kt
@@ -1,37 +1,49 @@
 import kotlinx.coroutines.*
 import net.dv8tion.jda.api.entities.Message
+import net.dv8tion.jda.api.entities.MessageChannel
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import java.lang.Exception
 
 val commandRegex = Regex("^!(\\w)+")
 
+fun sendMessage(channel: MessageChannel, message: String): CompletableDeferred<Message> {
+    val defer = CompletableDeferred<Message>()
+    channel.sendMessage(message).queue { res -> defer.complete(res) }
+    return defer
+}
+
 class CommandListener : ListenerAdapter() {
+    @ExperimentalCoroutinesApi
     override fun onMessageReceived(event: MessageReceivedEvent) {
         val msg = event.message
         val prefix = commandRegex.find(msg.contentRaw)
+        val channel = event.channel
 
         if (prefix == null || prefix.value.isEmpty()) {
             return
         }
 
-        val channel = event.channel
-
         when (prefix.value) {
             "!status" -> GlobalScope.launch {
                 // TODO fetch data here
-                var dataDeferred: Deferred<Int> = async {
-                    delay(3000)
-                    return@async 123
+                val data: Deferred<Int> = async {
+                    delay(300)
+                    return@async (Math.random() * 100).toInt()
                 }
-                println(dataDeferred)
-
-                channel.sendMessage("getting server status..")
-                    .queue { message: Message -> launch {
-                            val data: Int = dataDeferred.await()
-                            println(data)
-                            message.editMessage("done").queue()
-                        }
+                val message = sendMessage(channel, "getting server status..")
+                try {
+                    withTimeout(5000) {
+                        message.await().editMessage(data.await().toString()).queue()
                     }
+                } catch (e: TimeoutCancellationException) {
+                    if (message.isCompleted) {
+                        message.getCompleted().editMessage("took too long").queue()
+                    }
+                    println("!status took too long -- job cancelled")
+                } catch (e: Exception) {
+                    System.err.println(e.message)
+                }
             }
             "!ping" -> {
                 val time = System.currentTimeMillis()
@@ -44,6 +56,5 @@ class CommandListener : ListenerAdapter() {
                     }
             }
         }
-
     }
 }
